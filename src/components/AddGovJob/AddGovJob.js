81c259fdbf624b97b37fe3ffc39d268a28d8cb54
Shohanur Rahman Shaun <srshaon.dev@gmail.com>
2022-03-03 23:26:46 +0600

    resolved codes

--- src/components/AddGovJob/AddGovJob.js
+++ src/components/AddGovJob/AddGovJob.js
@@ -1,73 +1,54 @@
 import './AddGovJob.css';
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'
 import { useForm } from 'react-hook-form';
 import ModalMessage from './../ModalMessage/ModalMessage';
-
+import { useCreatePostMutation } from "../../Redux-handler/GovJobApiHandler";
+import { useHistory } from 'react-router';
 const AddGovJob = () => {
+  const [createJob, { isLoading, isSuccess }] = useCreatePostMutation()
+  const history = useHistory()
   // const{user,admin}=useAuth()
   const [show, setShow] = useState(false);
-  const handleShow = () => setShow(true);
-  const [imgUrl, setImgUrl] = useState('')
-  const [uploading, setUploading] = useState(false)
   const { register, reset, handleSubmit, formState: { errors } } = useForm();
-
   const onSubmit = data => {
+
     console.log(data)
     const info = {
-      place: data.organization,
-      title: data.position,
-      description: data.deadline,
+      organization: data.organization,
+      position: data.position,
+      deadline: data.deadline,
+      vacancy: data.vacancy,
       date: new Date().toLocaleString(),
-      // email:user.email,
-      img: imgUrl,
+
     }
-    axios.post('https://afternoon-headland-45054.herokuapp.com/addGovJob', info)
-      .then(response => {
-        console.log(response.data)
-        if (response.data.insertedId) {
-          handleShow()
-          reset()
-        }
-      });
-
+    createJob(info)
+    reset()
+    history.push('allgovjobs')
   }
 
-  // image upload
-  const handleFile = (e) => {
-    const fileInfo = e.target.files
-    const imgData = new FormData()
-    imgData.set("key", "cd8b0025cccd4015a70e149fd2ab8ad0")
-    imgData.append("image", fileInfo[0])
-    setUploading(true)
-    axios.post('https://api.imgbb.com/1/upload', imgData)
-      .then(response => {
-        setImgUrl(response.data.data.url)
-        setUploading(false)
-      });
-  }
-  console.log(imgUrl)
-  const productImageRegister = register("productImage",)
-  // {required: true}
+  useEffect(() => {
+    if (isSuccess) {
+      setShow(true)
+    }
+  }, [isSuccess])
+
+
   return (
     <>
       <ModalMessage show={show} setShow={setShow} message={'Submitted'} />
-      <div className="mt-5 d-flex justify-content-center row">
+      {isLoading && <p>Post creating.....</p>}
+      <div className="my-5 d-flex justify-content-center row">
         <div className="p-4 rounded col-lg-6 col-sm-10 shadow">
           <h2 className="text-center text-primary mb-4">Create A Gov Job</h2>
           <form onSubmit={handleSubmit(onSubmit)}>
             <input className="form-control mt-2" type="text" placeholder="Organization" {...register("organization", { required: true })} />
             <input className="form-control mt-2" type="text" placeholder="Position" {...register("position", { required: true })} />
             <input className="form-control mt-2" type="text" placeholder="Deadline" {...register("deadline", { required: true })} />
-            <input className="form-control mt-2"
-              type="file"
-              {...productImageRegister}
-              onChange={e => {
-                productImageRegister.onChange(e);
-                handleFile(e);
-              }} />
+            <input className="form-control mt-2" type="text" placeholder="Job link" {...register("joblink", { required: true })} />
+            <input className="form-control mt-2" type="number" placeholder="vacancy" {...register("vacancy", { required: true })} />
+
             {errors.exampleRequired && <span>This field is required</span>}
-            {uploading ? <p>Uploading....</p> : <input className="form-control mt-2 bg-primary" type="submit" />}
+            <input className="form-control mt-2 bg-primary" type="submit" />
           </form>
         </div>
       </div>
