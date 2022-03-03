6cc6a4c1f59882826dec3979737d4bb7daf6214a
Shohanur Rahman Shaun <srshaon.dev@gmail.com>
2022-03-04 05:06:24 +0600

    conditional form done

--- src/components/ProfileForm/ProfileForm3.js
+++ src/components/ProfileForm/ProfileForm3.js
@@ -4,26 +4,27 @@ import { withRouter } from "react-router-dom";
 import { useStateMachine } from "little-state-machine";
 import updateAction from "./updateAction";
 import clearAction from "./clearAction";
-import {useCreateProfileMutation } from "../../Redux-handler/ManageProfiles";
+import { useCreateProfileMutation } from "../../Redux-handler/ManageProfiles";
 import { useHistory } from 'react-router';
 const ProfileForm3 = (props) => {
-    const history=useHistory()
-    const[createProfile]=useCreateProfileMutation()
     const { register, handleSubmit } = useForm();
     const { actions, state } = useStateMachine({ clearAction });
     const onSubmit = (data) => {
-        const splitBio2 = state.data.bio.split(/\r?\n/g);
+        const splitBio2 = state?.data?.bio?.split(/\r?\n/g);
         const newData2 = { ...state.data, bio: splitBio2 };
-        console.log('form3',newData2);
-        console.log('form3',data);
-        console.log('form3',state);
-        createProfile(state.data)
-        
-        console.log('form3',actions);
+        console.log(newData2);
+        console.log(data);
+        console.log(state.data);
+        console.log(data);
+        console.log(state);
+        console.log(actions);
         actions.clearAction(data);
-        
-        console.log('form3',state);
-        console.log('form3',actions);
+        props.history.push("./result");
+        console.log(data);
+        console.log(state.data);
+        console.log(data);
+        console.log(state);
+        console.log(actions);
         // actions();
     };
     return (
