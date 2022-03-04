4459cfe250c78b14fe559f71aa03efe76a7201fa
Parveen Nuzhat <nuzhat.webdeveloper@gmail.com>
2022-03-04 19:40:35 +0600

    code update by Nuzhat

--- src/components/ManageJob/ManageJob.js
+++ src/components/ManageJob/ManageJob.js
@@ -1,13 +1,174 @@
-import './ManageJob.css';
+import axios from "axios";
+import React, { useState } from "react";
+import { Button, Card, Col, Modal } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
-import React from 'react';
+const ManageJob = ({ job, setJobs }) => {
+  const [show, setShow] = useState(false);
+  const handleState = (id) => {
+    const proceed = window.confirm("Are you sure that you want to update?");
+    if (proceed) {
+      const updatedJob = { ...job, status: "approved" };
+      const url = `https://afternoon-headland-45054.herokuapp.com/updateJob/${id}`;
+      axios.put(url, updatedJob).then((res) => {
+        if (res.data.modifiedCount) {
+          alert("updated successfully");
+        }
+      });
+    }
+  };
 
-const ManageJob = () => {
-    return (
-        <div>
-            <h2>Manage Job Created</h2>
-        </div>
-    );
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const { control, register, handleSubmit, reset } = useForm();
+
+  const onSubmit = (data) => {
+    const requirements = data.additionalRequirements.split(/\r?\n/g);
+    const newData = { ...data, additionalRequirements: requirements };
+    console.log(newData);
+    // const updatedJob = { ...job, newData };
+    // console.log(updatedJob);
+    axios
+      .put(`http://localhost:5000/jobs/${job._id}`, newData)
+      .then((res) => {
+        if (res.data.modifiedCount) {
+          alert("updated successfully");
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+  return (
+    <>
+      <Col md={12}>
+        <Card className="p-5" style={{ height: "75vh" }}>
+          <div>
+            <div className="row row-cols-lg-2 row-cold-md-2 d-md-flex align-items-center justify-content-center">
+              <Col md={4} className="">
+                <h5>{job?.category}</h5>
+                <img src={job.image} alt="" className="w-25 p-3" />
+                <h4>{job?.job}</h4>
+                <p>{job?.jobLocation}</p>
+                <p>{job?.employmentStatus}</p>
+                <p>${job?.salary}</p>
+                <p>{job?.vacancy}</p>
+                <p>Deadline: {job?.aplicationDeadline}</p>
+              </Col>
+              <Col md={8}>
+                <p>Educational Req: {job?.educationalRequirements}</p>
+                <p>Job Responsibilities: {job?.jobResponsibilities}</p>
+                <p>Additional Req:</p>
+                <ul>
+                  {job?.additionalRequirements.map((item) => (
+                    <li>{item}</li>
+                  ))}
+                </ul>
+              </Col>
+            </div>
+            <div className="text-center mt-4">
+              {job?.status?.toLowerCase() === "pending" && (
+                <button
+                  className="submit-btn p-3"
+                  onClick={() => handleState(job?._id)}
+                >
+                  Pending
+                </button>
+              )}
+              {job?.status?.toLowerCase() === "approved" && (
+                <Button
+                  className="submit-btn p-3"
+                  variant="primary"
+                  onClick={handleShow}
+                >
+                  Update Job
+                </Button>
+              )}
+            </div>
+          </div>
+        </Card>
+      </Col>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Update field of job details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="add-job py-5 w-100">
+            <form className="py-3" onSubmit={handleSubmit(onSubmit)}>
+              <input
+                {...register("company", { required: true })}
+                placeholder="Company name"
+                defaultValue={job.company}
+              />
+              <input
+                {...register("job", { required: true })}
+                placeholder="Job Title"
+                defaultValue={job.job}
+              />
+              <input
+                type="name"
+                {...register("jobLocation")}
+                placeholder="Location"
+                defaultValue={job.jobLocation}
+              />
+              <input
+                {...register("category", { required: true })}
+                placeholder="Category"
+                defaultValue={job.category}
+              />
+
+              <textarea
+                rows={4}
+                {...register("additionalRequirements", {
+                  required: true,
+                })}
+                placeholder="additional req"
+                defaultValue={job.additionalRequirements}
+              ></textarea>
+
+              <input
+                type="number"
+                {...register("vacancy")}
+                placeholder="vacancy"
+                defaultValue={job.vacancy}
+              />
+              <input
+                type="name"
+                {...register("educationalRequirements")}
+                placeholder="Education Req"
+                defaultValue={job.educationalRequirements}
+              />
+              <input
+                type="name"
+                {...register("experienceRequirements")}
+                placeholder="Experience Req"
+                defaultValue={job.experienceRequirements}
+              />
+              <input
+                type="name"
+                {...register("jobResponsibilities")}
+                placeholder="Job Responsibilities "
+                defaultValue={job.jobResponsibilities}
+              />
+
+              <input
+                type="number"
+                {...register("salary")}
+                placeholder="Salary $"
+                defaultValue={job.salary}
+              />
+              <input className="submit-btn p-2" type="submit" />
+            </form>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 };
 
 export default ManageJob;
--- src/components/ProfileForm/ProfileForm2.js
+++ src/components/ProfileForm/ProfileForm2.js
@@ -5,107 +5,143 @@ import { useStateMachine } from "little-state-machine";
 import updateAction from "./updateAction";
 
 const ProfileForm2 = (props) => {
-    const [condition, setCondition] = useState('admin');
-    const { register, handleSubmit } = useForm();
-    const { actions, state } = useStateMachine({ updateAction });
-    const onSubmit = (data) => {
-        console.log(data);
-        actions.updateAction(data);
-        const splitBio = data?.bio?.split(/\r?\n/g);
-        const newData = { ...data, bio: splitBio };
+  const [condition, setCondition] = useState("admin");
+  const { register, handleSubmit } = useForm();
+  const { actions, state } = useStateMachine({ updateAction });
+  const onSubmit = (data) => {
+    console.log(data);
+    actions.updateAction(data);
+    const splitBio = data?.bio?.split(/\r?\n/g);
+    const newData = { ...data, bio: splitBio };
 
-        console.log(newData);
-        // console.log(newData2);
-        console.log(data);
+    console.log(newData);
+    // console.log(newData2);
+    console.log(data);
 
-        props.history.push("./ProfileForm3");
-        console.log(data);
-        console.log(state.data);
-        console.log(data);
-        console.log(state);
-        console.log(actions);
-    };
-    return (
-        <div className='profile-form-div' >
+    props.history.push("./ProfileForm3");
+    console.log(data);
+    console.log(state.data);
+    console.log(data);
+    console.log(state);
+    console.log(actions);
+  };
+  return (
+    <div className="profile-form-div">
+      <div>
+        {condition === "admin" && (
+          <div>
+            <div className="profile-form-div">
+              <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div class="profile-form-container">
+                    <div class="profile-form-inputs">
+                      <div className="profile-form-input-pair d-flex justify-content-center">
+                        <textarea
+                          rows={8}
+                          className="profile-form2-bio-textarea"
+                          placeholder="About Company Culture"
+                          defaultValue=""
+                          {...register("acc", { required: true })}
+                        />
+                      </div>
+                      <br />
+                      <div className="profile-form-input-pair d-flex justify-content-center">
+                        <textarea
+                          rows={5}
+                          className="profile-form2-hobby-textarea"
+                          placeholder="Company Vision"
+                          defaultValue=""
+                          {...register("vision", { required: true })}
+                        />
+                      </div>
+                      <br />
+                      <div className="profile-form-input-pair d-flex justify-content-center">
+                        <textarea
+                          rows={5}
+                          className="profile-form2-hobby-textarea"
+                          placeholder="Achievements / Awards"
+                          defaultValue=""
+                          {...register("achievements", { required: true })}
+                        />
+                      </div>
 
-
-            <div >
-                {
-                    (condition === 'admin') && (
-                        <div>
-                            <div className='profile-form-div' >
-                                <div >
-                                    <form onSubmit={handleSubmit(onSubmit)}>
-                                        <div class="profile-form-container">
-
-                                            <div class="profile-form-inputs">
-                                                <div className='profile-form-input-pair d-flex justify-content-center'>
-
-                                                    <textarea rows={8} className='profile-form2-bio-textarea' placeholder="About Company Culture" defaultValue="" {...register("acc", { required: true })} />
-                                                </div>
-                                                <br />
-                                                <div className='profile-form-input-pair d-flex justify-content-center'>
-
-                                                    <textarea rows={5} className='profile-form2-hobby-textarea' placeholder="Company Vision" defaultValue="" {...register("vision", { required: true })} />
-                                                </div>
-                                                <br />
-                                                <div className='profile-form-input-pair d-flex justify-content-center'>
-
-                                                    <textarea rows={5} className='profile-form2-hobby-textarea' placeholder="Achievements / Awards" defaultValue="" {...register("achievements", { required: true })} />
-                                                </div>
-
-
-                                                <div className='profile-form-button-div d-flex justify-content-center' >
-                                                    <button className='profile-form-button' type="submit">Next</button>
-                                                </div>
-                                            </div>
-
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-
-                        </div>)
-                }
-                {
-                    (condition === 'user') && (
-                        <div>
-                            <form onSubmit={handleSubmit(onSubmit)}>
-                                <div class="profile-form-container">
-
-                                    <div class="profile-form-inputs">
-                                        <div className='profile-form-input-pair d-flex justify-content-center'>
-
-                                            <textarea rows={8} className='profile-form2-bio-textarea' placeholder="Your Bio" defaultValue="" {...register("bio", { required: true })} />
-                                        </div>
-                                        <br />
-                                        <div className='profile-form-input-pair d-flex justify-content-center'>
-
-                                            <textarea rows={5} className='profile-form2-hobby-textarea' placeholder="Extra Curricular Activities / Hobbies" defaultValue="" {...register("hobbies", { required: true })} />
-                                        </div>
-                                        <br />
-                                        <div className='profile-form-input-pair d-flex justify-content-center'>
-
-                                            <textarea rows={5} className='profile-form2-hobby-textarea' placeholder="Achievements / Awards" defaultValue="" {...register("achievements", { required: true })} />
-                                        </div>
-                                        <div className='profile-form-input-pair d-flex justify-content-center'>
-                                            <input className='profile-form-input' placeholder="Job Preference" defaultValue={''} {...register("preference", { required: true })} />
-                                            <input className='profile-form-input' placeholder="Available For" defaultValue={''} {...register("available", { required: true })} />
-                                        </div>
-
-                                        <div className='profile-form-button-div d-flex justify-content-center' >
-                                            <button className='profile-form-button' type="submit">Next</button>
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </form>
-                        </div>
-                    )
-                }
+                      <div className="profile-form-button-div d-flex justify-content-center">
+                        <button
+                          className="profile-form-button p-3"
+                          type="submit"
+                        >
+                          Next
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
             </div>
-        </div>
-    );
+          </div>
+        )}
+        {condition === "user" && (
+          <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div class="profile-form-container mt-3 mb-5">
+                <div class="profile-form-inputs">
+                  <div className="profile-form-input-pair d-flex justify-content-center">
+                    <textarea
+                      rows={8}
+                      className="profile-form2-bio-textarea"
+                      placeholder="Your Bio"
+                      defaultValue=""
+                      {...register("bio", { required: true })}
+                    />
+                  </div>
+                  <br />
+                  <div className="profile-form-input-pair d-flex justify-content-center">
+                    <textarea
+                      rows={5}
+                      className="profile-form2-hobby-textarea"
+                      placeholder="Extra Curricular Activities / Hobbies"
+                      defaultValue=""
+                      {...register("hobbies", { required: true })}
+                    />
+                  </div>
+                  <br />
+                  <div className="profile-form-input-pair d-flex justify-content-center">
+                    <textarea
+                      rows={5}
+                      className="profile-form2-hobby-textarea"
+                      placeholder="Achievements / Awards"
+                      defaultValue=""
+                      {...register("achievements", { required: true })}
+                    />
+                  </div>
+                  <div className="profile-form-input-pair d-flex justify-content-center">
+                    <input
+                      className="profile-form-input"
+                      placeholder="Job Preference"
+                      defaultValue={""}
+                      {...register("preference", { required: true })}
+                    />
+                    <input
+                      className="profile-form-input"
+                      placeholder="Available For"
+                      defaultValue={""}
+                      {...register("available", { required: true })}
+                    />
+                  </div>
+
+                  <div className="profile-form-button-div d-flex justify-content-center">
+                    <button className="profile-form-button p-3" type="submit">
+                      Next
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ProfileForm2;
