35b9537072f6d9414c79f6c3be62a75423aa1b1a
Razibul-Alam <Razibul-Alam@users.noreply.github.com>
2022-03-08 08:51:15 +0600

    company-profile done

---
+++ src/components/CompanyProfile/CategorySelector.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ButtonGroup } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+
+const CategorySelector = ({setCategory,companies}) => {
+    return (
+        <div>
+            <ButtonGroup aria-label="Basic example">
+  {companies?.map(company=><Button className='mx-2' onClick={()=>setCategory(company?.industry)} variant="primary">{company?.industry}</Button>)}
+</ButtonGroup>
+        </div>
+    );
+};
+
+export default CategorySelector;
