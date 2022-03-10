0db59e98a6598e7a2b4abe3aa1829f2dcdc46788
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2022-03-11 02:02:29 +0600

    Fixed Minor

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import useAuth from '../../hooks/useAuth';
 import { PieChart, Pie, Tooltip, ResponsiveContainer, LabelList, Label, Cell } from 'recharts';
 
