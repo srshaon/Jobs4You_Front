7bd8d102b6481ff718316076ea010df5c4ac7771
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2022-03-11 00:55:12 +0600

    Dynamic Chart Feature added

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useAuth from '../../hooks/useAuth';
 import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer, LabelList, Label, Cell } from 'recharts';
 
 const Chart = () => {
-    const data = [
-        { name: 'HTML5', value: 400 },
-        { name: 'CSS3', value: 300 },
-        { name: 'JavaScript', value: 300 },
-        { name: 'React', value: 200 },
-        { name: 'Testing', value: 278 },
-        { name: 'Deployment', value: 189 },
-      ];
-    const data2 = [
-        { name: 'acquired', value: 1200 },
-        { name: 'remaining', value: 467 }
-    ];
+    const [postedSkills, setPostedSkills] = useState([]);
+    const [gainedSkills, setGainedSkills] = useState([]);
+    const [data1, setData1] = useState([]);
+    const [data2, setData2] = useState([]);
+    const { email } = useAuth();
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/posted-skills/:${_id}`)
+        .then(res => res.json())
+        .then(data => setPostedSkills(data));
+    }, []);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/skills/:${email}`)
+        .then(res => res.json())
+        .then(data => setGainedSkills(data));
+    }, []);
+
+    useState(() => {
+        if (postedSkills && gainedSkills) {
+            postedSkills.forEach(postedSkill => {
+                let index = gainedSkills.findIndex(gainedSkill => gainedSkill.includes(postedSkill));
+                if (index !== -1) {
+                    let tmp = [...data1, { name: postedSkill, value: 1 }];
+                    setData1(tmp);
+                }
+                else if (index === -1) {
+                    let tmp = [...data2, { name: postedSkill, value: 1 }];
+                    setData2(tmp);
+                }
+            });
+        }
+    });
+
     const colors = [ 'rgb(130, 202, 157)', '#ec7272' ];
     return (
         <div style={{width: '100%', height: '80vh', color: 'black'}}>
@@ -22,7 +45,7 @@ const Chart = () => {
                 <Pie
                     dataKey="value"
                     isAnimationActive={true}
-                    data={data}
+                    data={data1}
                     cx="50%"
                     cy="50%"
                     innerRadius={40}
@@ -33,7 +56,7 @@ const Chart = () => {
                 {/* <LabelList dataKey="name" position="outside" offset={40} stroke="black" fontSize="smaller" />s */}
                 </Pie>
                 <Pie data={data2} dataKey="value" cx="50%" cy="50%" innerRadius={110} outerRadius={135} fill="#82ca9d" label>
-                    {data.map((entry, index) => (
+                    {data1.map((entry, index) => (
                         <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
                     ))}
                 </Pie>
