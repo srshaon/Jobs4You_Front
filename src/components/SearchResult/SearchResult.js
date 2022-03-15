517cc75a282e76f9b6f320452fd0c17c9aae8d59
Shohanur Rahman Shaun <srshaon.dev@gmail.com>
2022-03-15 12:30:28 +0600

    search result

---
+++ src/components/SearchResult/SearchResult.js
@@ -0,0 +1,13 @@
+import './SearchResult.css';
+
+import React from 'react';
+
+const SearchResult = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default SearchResult;
