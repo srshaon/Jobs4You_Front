148d6cdadb1de805d0301e90da71baa6defbd3ac
Razibul-Alam <Razibul-Alam@users.noreply.github.com>
2022-03-27 19:36:38 +0600

    replace localhost to server

--- src/Redux-handler/ManageProfiles.js
+++ src/Redux-handler/ManageProfiles.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const profilesApi = createApi({
   reducerPath: "profilesApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/",
+    baseUrl: "https://afternoon-headland-45054.herokuapp.com/",
   }),
 
   tagTypes: ["Profiles"],
