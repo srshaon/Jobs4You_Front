cf30e8439ebcb69142048fae82b753d42309a8ff
Parveen Nuzhat <nuzhat.webdeveloper@gmail.com>
2022-03-29 01:55:46 +0600

    code pushed by Nuzhat

--- src/components/AllJobs/AllJobs.js
+++ src/components/AllJobs/AllJobs.js
@@ -1,25 +1,22 @@
-import './AllJobs.css';
+import "./AllJobs.css";
 
-import React, { useEffect, useState } from 'react';
-
+import React, { useEffect, useState } from "react";
 
 const AllJobs = () => {
-
-    const [pqr, setPqr] = useState([]);
-    useEffect(() => {
-        fetch("https://afternoon-headland-45054.herokuapp.com/alljobs")
-            .then((res) => res.json())
-            .then(data => {
-                console.log(data)
-                setPqr(data)
-            })
-    })
-    return (
-
-        <div className='abc'>
-            <h3>{pqr.length}</h3>
-        </div>
-    );
+  const [pqr, setPqr] = useState([]);
+  useEffect(() => {
+    fetch("https://afternoon-headland-45054.herokuapp.com/alljobs")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPqr(data);
+      });
+  });
+  return (
+    <div className="abc">
+      <h3>{pqr.length}</h3>
+    </div>
+  );
 };
 
 export default AllJobs;
